4aae664de6e120ec68cd52b858a2d1873f9d5ebb
OroshiX <oroshix@users.noreply.github.com>
2022-04-03 12:50:33 +0200

    [WIP] add actionType to resource

--- Assets/ScriptableObjects/ResourceCharacteristics.cs
+++ Assets/ScriptableObjects/ResourceCharacteristics.cs
@@ -2,12 +2,14 @@
 // 
 // Created by jessi on 2022-04-02
 
+using Player.ResourcesUsable;
 using UnityEngine;
 
 namespace ScriptableObjects {
 [CreateAssetMenu(fileName = "InventoryItemCharacteristics", menuName = "New Inventory item")]
 public class ResourceCharacteristics : ScriptableObject {
     public bool consumable;
+    public ResourceType actionType = ResourceType.OnFire;
     public int spawnWait = 2;
     public int activeDuration = 1;
     public int cooldown = 3;
---
+++ Assets/Scripts/Player/ResourcesUsable/ResourceType.cs
@@ -0,0 +1,10 @@
+﻿// Project Assembly-CSharp
+// 
+// Created by jessi on 2022-04-03
+
+namespace Player.ResourcesUsable {
+public enum ResourceType {
+    OnFire,
+    SaveCivil
+}
+}
