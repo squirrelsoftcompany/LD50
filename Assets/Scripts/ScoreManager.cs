63bc75cb41e53b7a08b7e210dce52dad38e53d11
OroshiX <oroshix@users.noreply.github.com>
2022-04-04 20:07:05 +0200

    [FCT] Keep track of nb of civilian saved (and put in ScoreManager)

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using GameEventSystem;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
     private int m_score = 0;
     public int Score => m_score;
+    [SerializeField] private GameEvent civilianSavedEvent;
 
     public GameEventSystem.GameEvent m_onScoreUpdated = null;
+
+    public int nbCivilianSaved => civilianSavedEvent.sentInt;
 
     // Start is called before the first frame update
     void Start()
