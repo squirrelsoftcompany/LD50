d9d417b180e3e0c2e44270dfc046baea8d7cad27
OroshiX <oroshix@users.noreply.github.com>
2022-04-04 16:54:05 +0200

    [BUG] Do not lose multiple times

--- Assets/Scripts/Player/ResourcesUsable/HumanFighter.cs
+++ Assets/Scripts/Player/ResourcesUsable/HumanFighter.cs
@@ -8,8 +8,8 @@ using UnityEngine;
 namespace Player.ResourcesUsable {
 public abstract class HumanFighter : Resource, IMortal {
     protected Animator animator;
-    protected int currentFireExposed;
     protected int amountFireExposed;
+    private bool hasLost;
     private static readonly int Death = Animator.StringToHash("Death");
     [SerializeField] protected GameEvent deathEvent;
     public abstract int criticalAmountSurvivable();
@@ -20,15 +20,16 @@ public abstract class HumanFighter : Resource, IMortal {
     }
 
     public void newFireIntensity(int intensity) {
-        if(intensity ==0) return;
+        if (hasLost) return;
+        if (intensity == 0) return;
         amountFireExposed += intensity;
         if (amountFireExposed >= criticalAmountSurvivable()) {
             loseSadly();
             animator.SetTrigger(Death);
+            hasLost = true;
         }
     }
 
     public abstract void doDie();
-
 }
 }
