473a19de52e3f069e2eebd872e0180a724b7aa2f
OroshiX <oroshix@users.noreply.github.com>
2022-04-04 21:09:01 +0200

    [FIX] Forward animation to parent

--- Assets/Scripts/Player/OnlyForwarding.cs
+++ Assets/Scripts/Player/OnlyForwarding.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 namespace Player {
 public class OnlyForwarding : MonoBehaviour {
     private IMortal _mortal;
+    private Civilian _civilian;
     private void Awake() {
         _mortal = GetComponentInParent<IMortal>();
+        _civilian = GetComponentInParent<Civilian>();
     }
 
     public void doDie() {
         _mortal.doDie();
+    }
+
+    public void onSaveAnimEnded() {
+        if (_civilian != null) {
+            _civilian.onSaveAnimEnded();
+        }
     }
 }
 }
