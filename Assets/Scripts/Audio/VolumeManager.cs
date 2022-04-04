20d16270060b3490891208f9f7249f49655eb5a7
Jean-François Erdelyi <jferdelyi@gmail.com>
2022-04-05 01:07:38 +0200

    [FCT] Add volume manager

---
+++ Assets/Scripts/Audio/VolumeManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeManager: MonoBehaviour {
+    FMOD.Studio.EventInstance instance;
+    FMOD.Studio.Bus sfxBus;
+    FMOD.Studio.Bus musiqueBus;
+    FMOD.Studio.Bus masterBus;
+
+
+    [SerializeField][Range(-80f, 10f)] 
+    private float sfxBusVolume;
+    [SerializeField][Range(-80f, 10f)] 
+    private float musiqueBusVolume;
+    [SerializeField][Range(0.0f, 1f)] 
+    private float masterBusVolume = 1.0f;
+
+    private float sfxVolume;
+    private float musiqueVolume;
+
+    void Start() {
+        sfxBus = FMODUnity.RuntimeManager.GetBus("bus:/SFX");
+        musiqueBus = FMODUnity.RuntimeManager.GetBus("bus:/Musique");
+    }
+
+    void Update() {
+        sfxVolume = Mathf.Pow(10.0f, (sfxBusVolume - (90f * (1 - masterBusVolume))) / 20f);
+        sfxBus.setVolume(sfxVolume);
+
+        musiqueVolume = Mathf.Pow(10.0f, (musiqueBusVolume - (90f * (1 - masterBusVolume))) / 20f);
+        musiqueBus.setVolume(musiqueVolume);
+    }
+}
