8f4c1691a42374113762b40738c822eaca86a0b4
Jean-François Erdelyi <jferdelyi@gmail.com>
2022-04-04 14:54:35 +0200

    [FCT] Update camera with the music manager

---
+++ Assets/Scripts/Audio/MusicManager.cs
@@ -0,0 +1,33 @@
+using System;
+using FMODUnity;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public float chillLevel = 0.0f;
+    public float tenseLevel = 0.3f;
+    public float hardlLevel = 0.5f;
+
+    private FMODUnity.StudioEventEmitter emitter;
+    private Environment.World world;
+
+    // Start is called before the first frame update
+    void Start() {
+        emitter = GetComponent<FMODUnity.StudioEventEmitter>();
+        world = Environment.World.Inst;
+    }
+
+    // Called when the fire intensify value change
+    public void fireIntensity() {
+        float fire = world.m_worldIsOnFire;
+        int value = 1;
+
+        if(fire > hardlLevel) {
+            value = 3;
+        } else if(fire > tenseLevel) {
+            value = 2;
+        }
+
+        emitter.SetParameter("Fire", value);
+    }
+}
